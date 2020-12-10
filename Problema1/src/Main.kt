babb523250596667582c0a50371f16b2cc53036a
Carlos <usarral@github.com>
2020-12-10 18:01:34 +0100

    Problema 1 2020-12-10

---
+++ Problema1/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(parametro: Array<String>) {
+        val valor1: Int
+        val valor2: Int
+        valor1 = 100
+        valor2 = 400
+        var resultado: Int
+        resultado = valor1 + valor2
+        println("La suma de $valor1 + $valor2 es $resultado")
+        resultado = valor1 * valor2
+        println("El producto de $valor1 * $valor2 es $resultado")
+}
